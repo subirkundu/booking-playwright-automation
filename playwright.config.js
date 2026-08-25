5576cf08a567d72466e7f584d8e1dd590f22c6e2
subir-kundo-qa <kundosubir@gmail.com>
2026-08-26 00:25:12 +0600

    change in config desktop to mobile

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     headless: false, 
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
-    //...devices ['iPhone 15 Pro Max']
-    viewport: { width: 1920, height: 1080 },
+    ...devices ['iPhone 15 Pro Max']
+    //viewport: { width: 1920, height: 1080 },
   },
 });
