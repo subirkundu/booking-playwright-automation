800236cd4d0e720d0abfc49e07adc9312b94b362
subir-kundo-qa <kundosubir@gmail.com>
2026-08-25 23:00:51 +0600

    Update hotel booking test

--- tests/bookingUAT.spec.js
+++ tests/bookingUAT.spec.js
@@ -3,7 +3,7 @@ const { HotelBook } = require('../pageobjects/HotelBook');
 const { FlightBook } = require('../pageobjects/FlightBook');
 const { AttractionsBook } = require('../pageobjects/AttractionsBook');
 
-test.only('Hotel Booking UAT', async ({ page }) => {
+test('Hotel Booking UAT', async ({ page }) => {
 
   const numberOfChildren = 3;
   const ageDropdowns = page.getByRole('combobox');
