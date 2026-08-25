e4a62172ed26527df8b5d2ed532d5bb32fe77e01
subir-kundo-qa <kundosubir@gmail.com>
2026-08-26 00:08:26 +0600

    Update in Main & Config

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   reporter: 'html',
 
   use: {
-    browserName: 'firefox',
+    browserName: 'webkit',
     headless: false, 
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
