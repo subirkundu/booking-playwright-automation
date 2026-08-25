d7b84f1ad0940ca4fa935e5dcbeb22d8c1845dfa
subir-kundo-qa <kundosubir@gmail.com>
2026-08-25 23:42:13 +0600

    change in config file

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 
   use: {
     browserName: 'chromium',
-    headless: true, 
+    headless: false, 
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
     //...devices ['iPhone 15 Pro Max']
